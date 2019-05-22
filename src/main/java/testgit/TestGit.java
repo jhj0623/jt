ade2e80897583f8fdc4d7a6135c7c588869cff12
jhj0623 <291907856@qq.com>
2019-05-22 19:41:20 +0800

    test commit

---
+++ src/main/java/testgit/TestGit.java
@@ -0,0 +1,10 @@
+package testgit;
+
+/**
+ * Created by Administrator on 2019/5/22.
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("试试看我的git提交成功了没有");
+    }
+}
