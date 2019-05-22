578c0a53b15b7e9d993d8af8eca511749b189f52
jerry <48677379+jhj0623@users.noreply.github.com>
2019-05-22 21:27:04 +0800

    update
    update this class

--- src/main/java/testgit/TestGit.java
+++ src/main/java/testgit/TestGit.java
@@ -5,6 +5,6 @@ package testgit;
  */
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("试试看我的git提交成功了没有");
+        System.out.println("我的git提交成功了,我现在要把它拉下来");
     }
 }
